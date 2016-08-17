a4cb9d7ca345e888d0261370acc6f5cbc84e9d6b
Alphys <je.park@memebox.com>
2016-08-17 19:01:56 +0900

    Actionbar drawer indicator adding. (working)

--- app/src/main/java/com/stewhouse/tweety/TMainActivity.java
+++ app/src/main/java/com/stewhouse/tweety/TMainActivity.java
@@ -1,26 +1,64 @@
 package com.stewhouse.tweety;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
-import android.view.ViewStub;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import com.stewhouse.tweety.utility.TAppCompatActivity;
+public class TMainActivity extends AppCompatActivity {
 
-public class TMainActivity extends TAppCompatActivity {
-
+    private DrawerLayout mDrawerLayout = null;
     private ListView mDrawerList = null;
+    private ActionBarDrawerToggle mDrawerToggle = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Use TAppCompatActivity's content_viewstub to show TAppCompatActivity's content.
-        ViewStub viewStub = (ViewStub) findViewById(R.id.content_viewstub);
-        viewStub.setLayoutResource(R.layout.activity_main);
-        viewStub.inflate();
+        setContentView(R.layout.activity_main);
+
+        // Setting ToolBar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                toolbar,  /* nav drawer icon to replace 'Up' caret */
+                0,  /* "open drawer" description */
+                0  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle("close");
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle("open");
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
     }
 
     private void initDrawer() {
@@ -33,8 +71,8 @@ public class TMainActivity extends TAppCompatActivity {
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                     if (position == 0) {    // Profile Cell.
-                        Intent intent = new Intent(TMainActivity.this, TProfileActivity.class);
-                        startActivity(intent);
+//                        Intent intent = new Intent(TMainActivity.this, TProfileActivity.class);
+//                        startActivity(intent);
                     }
                 }
             });
